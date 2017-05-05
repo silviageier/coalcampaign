100e2611edf9363fc12abcdf0974dc54073cbc97
miskakulmala <miska.kulmala@aalto.fi>
2017-05-05 18:13:07 +0300

    yeah

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ var gameTitle = function(game){}
  
 gameTitle.prototype = {
   	create: function(){
-		var playButton = this.game.add.button(game.world.centerX - 95, 400, 'button', playTheGame, this, 2, 1, 0););
+		var playButton = this.game.add.button(game.world.centerX - 95, 400, 'button', playTheGame, this););
 		playButton.anchor.setTo(0.5,0.5);
 	},
 	playTheGame: function(){
